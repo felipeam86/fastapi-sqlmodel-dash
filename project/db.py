9f5b2bbeb3bf95d0801912ae43fc92c2d2b940e5
Felipe Aguirre Martinez <felipeam86@gmail.com>
2021-11-23 16:14:15 +0200

    Add function to query with Pandas

--- project/db.py
+++ project/db.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sqlmodel import SQLModel, create_engine
 
 sql_url = "postgresql://root:root@localhost:5432/sales"
@@ -6,3 +7,10 @@ engine = create_engine(sql_url)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
+def pandas_query(sql):
+    with engine.connect() as conn:
+        df = pd.read_sql(sql, con=conn)
+
+    return df
