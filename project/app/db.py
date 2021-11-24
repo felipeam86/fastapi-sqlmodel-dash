17affc97a66b86f342be50fa4e977929da019728
Felipe Aguirre Martinez <felipeam86@gmail.com>
2021-11-24 18:01:45 +0200

    Remove pandas dependency from API

--- project/app/db.py
+++ project/app/db.py
@@ -1,6 +1,5 @@
 import os
 
-import pandas as pd
 from sqlmodel import SQLModel, create_engine
 
 sql_url = os.environ.get("DATABASE_URL")
@@ -9,10 +8,3 @@ engine = create_engine(sql_url)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-
-def pandas_query(sql):
-    with engine.connect() as conn:
-        df = pd.read_sql(sql, con=conn)
-
-    return df
